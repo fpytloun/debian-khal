4e4a0a066ac93f2a2dbc95f1bccbccf99061303a
Filip Pytloun <filip@pytloun.cz>
2016-08-12 21:18:33 +0200

    Avoid privacy breach in sphinx doc

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -108,11 +108,11 @@ html_theme = 'alabaster'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {
-    'github_user': 'pimutils',
-    'github_repo': 'khal',
-    'travis_button': 'true',
-}
+#html_theme_options = {
+#    'github_user': 'pimutils',
+#    'github_repo': 'khal',
+#    'travis_button': 'true',
+#}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
